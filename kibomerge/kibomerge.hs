449acbd1e043a6f380ed6f5a3d3b148999ba2893
Kazuki Iida <kazuki.iida@liferobotics.jp>
2017-08-09 11:18:44 +0900

    kibomerge.hsのコメントに間違いがあったので修正。

--- kibomerge/kibomerge.hs
+++ kibomerge/kibomerge.hs
@@ -8,11 +8,12 @@ counting :: [[String]] -> [[String]]
 counting = sortBy (\x y -> head x `compare` head y)
          . map (\(x,y) -> [intercalate " " $ sort y, x, show $ length y]) -- タプルからリストに戻す
          . toAscList -- マップからリストに戻す
-         . fromListWith (++) -- 重複が発生したらQtyを加算しながらmap化
+         . fromListWith (++) -- 重複が発生したらリファレンスを追加しながらmap化
          . foldl (\acc (x:xs) -> (head xs,[x]) : acc ) [] -- リストの先頭を取り出してタプルに変換
 
 addPostfix :: String -> FilePath -> FilePath
 addPostfix p file = takeBaseName file ++ p ++ takeExtension file
+
 main = do
   args <- getArgs
   let file = head args
